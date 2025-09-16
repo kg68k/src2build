9354b448b55e3b9ae7fb93979d185c2d00380956
kg68k <78926718+kg68k@users.noreply.github.com>
2025-09-17 01:29:54 +0900

    chore: version 1.0.0

--- src/src2build.c
+++ src/src2build.c
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-static const char ProgramAndVersion[] = "src2build 0.0.0";
+static const char ProgramAndVersion[] = "src2build 1.0.0";
 static const char Copyright[] = "2025 TcbnErik";
 
 #include <dirent.h>
